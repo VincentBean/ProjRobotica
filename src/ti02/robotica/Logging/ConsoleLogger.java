d2c8b7299fe1678a5ed2b332d637cab84c3f5028
Luuk3333 <Luuk3333@users.noreply.github.com>
2019-02-28 13:36:53 +0100

    Add timestamp to logger

--- src/ti02/robotica/Logging/ConsoleLogger.java
+++ src/ti02/robotica/Logging/ConsoleLogger.java
@@ -1,34 +1,42 @@
 package ti02.robotica.Logging;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class ConsoleLogger implements ILogger {
 
     @Override
     public void Debug(String msg) {
-        write("[DEBUG] " + msg);
+        write(msg);
     }
 
     @Override
     public void Info(String msg) {
-        write("[INFO] " + msg);
+        write(msg);
     }
 
     @Override
     public void Warn(String msg) {
-        write("[WARN] " + msg);
+        write(msg);
     }
 
     @Override
     public void Error(String msg) {
-        write("[ERROR] " + msg);
+        write(msg);
     }
 
     @Override
     public void Error(Exception ex) {
-        write("[ERROR] Exception: " + ex.getMessage());
+        write("Exception: " + ex.getMessage());
         write("Stacktrace: \n" + ex.getStackTrace().toString());
     }
 
     private void write(String msg) {
-        System.out.println(msg);
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+
+        String calling =  Thread.currentThread().getStackTrace()[2].getMethodName().toUpperCase();
+
+        System.out.println("[" + sdf.format(cal.getTime()) + " " + calling + "] " + msg);
     }
 }
