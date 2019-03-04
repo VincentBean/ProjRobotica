f12f64a06f830306197b7c122c6a7252b1424e1d
Luuk3333 <Luuk3333@users.noreply.github.com>
2019-03-04 11:41:08 +0100

    Add status percentage

--- src/ti02/robotica/Models/MMPicture.java
+++ src/ti02/robotica/Models/MMPicture.java
@@ -45,12 +45,15 @@ public class MMPicture {
         int bounds[] = {-1, -1, outputImage.getHeight(), 0};    // left, right, top, down
 
         int totalPixelCount = outputImage.getWidth() * outputImage.getHeight();
+        int pixelCount = 1;
 
         for (int x = 0; x < getWidth(); x++)
         {
 
             for (int y = 0; y < getHeight(); y++)
             {
+                CurrentLogger.Logger.Debug((float)((int)(((float)pixelCount++ / (float)totalPixelCount) * 10000.0))/100.0 + "%");
+
                 final int finalX = x;
                 final int finalY = y;
 
