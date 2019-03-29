41eb8d377364895eff72323e7c4e6d22617aac5a
Luuk3333 <Luuk3333@users.noreply.github.com>
2019-03-29 18:56:44 +0100

    Add info messages

--- src/ti02/robotica/PhotoDetection/Camera.java
+++ src/ti02/robotica/PhotoDetection/Camera.java
@@ -11,19 +11,23 @@ import java.io.IOException;
 public class Camera {
 
     public BufferedImage TakePicture() {
+        CurrentLogger.Logger.Info("Taking picture..");
         BufferedImage output = null;
+        long start = System.currentTimeMillis();
 
         try {
             // https://github.com/Hopding/JRPiCam
             RPiCamera piCamera = new RPiCamera("/tmp");
 
             piCamera.setWidth(512).setHeight(512)   // Set Camera to produce 500x500 images.
-                    .setBrightness(75)              // Adjust Camera's brightness setting.
+                    .setBrightness(50)              // Adjust Camera's brightness setting.
                     .setExposure(Exposure.SPORTS)   // Set Camera's exposure.
                     .setTimeout(2)                  // Set Camera's timeout.
                     .setAddRawBayer(true);          // Add Raw Bayer data to image files created by Camera.
 
             output = piCamera.takeBufferedStill();
+
+            CurrentLogger.Logger.Info("Finished taking picture. Took " + (System.currentTimeMillis() - start) + " ms.");
 
         } catch (FailedToRunRaspistillException | InterruptedException | IOException e) {
             CurrentLogger.Logger.Error("Error: could not take picture.");
