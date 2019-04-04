77f94b01a68774cdac1a1ff70d66f52cc96c5f7d
Luuk3333 <Luuk3333@users.noreply.github.com>
2019-04-04 13:50:06 +0200

    Remove RPiCamera

--- src/ti02/robotica/PhotoDetection/Camera.java
+++ src/ti02/robotica/PhotoDetection/Camera.java
@@ -1,11 +1,10 @@
 package ti02.robotica.PhotoDetection;
 
-import com.hopding.jrpicam.RPiCamera;
-import com.hopding.jrpicam.enums.Exposure;
-import com.hopding.jrpicam.exceptions.FailedToRunRaspistillException;
 import ti02.robotica.Logging.CurrentLogger;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 public class Camera {
@@ -16,20 +15,11 @@ public class Camera {
         long start = System.currentTimeMillis();
 
         try {
-            // https://github.com/Hopding/JRPiCam
-            RPiCamera piCamera = new RPiCamera("/tmp");
-
-            piCamera.setWidth(512).setHeight(512)   // Set Camera to produce 500x500 images.
-                    .setBrightness(50)              // Adjust Camera's brightness setting.
-                    .setExposure(Exposure.SPORTS)   // Set Camera's exposure.
-                    .setTimeout(2)                  // Set Camera's timeout.
-                    .setAddRawBayer(true);          // Add Raw Bayer data to image files created by Camera.
-
-            output = piCamera.takeBufferedStill();
+            output = ImageIO.read(new File("input/m_en_m_rood.jpeg"));
 
             CurrentLogger.Logger.Info("Finished taking picture. Took " + (System.currentTimeMillis() - start) + " ms.");
 
-        } catch (FailedToRunRaspistillException | InterruptedException | IOException e) {
+        } catch (IOException e) {
             CurrentLogger.Logger.Error("Error: could not take picture.");
             CurrentLogger.Logger.Error(e);
             e.printStackTrace();
