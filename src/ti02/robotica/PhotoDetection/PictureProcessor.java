52e34995c42fc9fbb558cc7c6c9d8d9c010b823c
Luuk3333 <Luuk3333@users.noreply.github.com>
2019-04-11 12:31:47 +0200

    Add detection of color green

--- src/ti02/robotica/PhotoDetection/PictureProcessor.java
+++ src/ti02/robotica/PhotoDetection/PictureProcessor.java
@@ -18,6 +18,9 @@ public class PictureProcessor {
         MMPicture mmPicture = new MMPicture(source.getWidth(), source.getHeight());
 
         final int colorMM = 0x804b004b; // AARRGGBB
+        final int colorRed = 0x80FF0000;
+        final int colorGreen = 0x8000FF00;
+        final int colorBlue = 0x800000FF;
 
         for (int x = 0; x < source.getWidth(); x++)
         {
@@ -28,6 +31,15 @@ public class PictureProcessor {
 
                 if (rgb[0] >= 127 && rgb[1] >= 127 && rgb[2] >= 127) {
                     mmPicture.setPixel(x, y, colorMM);
+                }
+//
+//                if (rgb[0] >= 100 && rgb[0] <= 150 && rgb[1] <= 50 && rgb[2] <= 75) {
+//                    mmPicture.setPixel(x, y, colorRED);
+//                }
+
+
+                if (rgb[0] <= 100 && rgb[1] >= 100 && rgb[2] <= 100) {
+                    mmPicture.setPixel(x, y, colorGreen);
                 }
 
                 // Todo: determine bounds
